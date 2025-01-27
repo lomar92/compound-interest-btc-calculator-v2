4ef1011e413eb6e5dda0eaac92b3ae0dfc58dc4d
Amar Lojo {he/him} <53077361+lomar92@users.noreply.github.com>
2025-01-27 21:56:12 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'v0 App',
-  description: 'Created with v0',
+  description: 'Retirement 4%',
 }
 
 export default function RootLayout({
