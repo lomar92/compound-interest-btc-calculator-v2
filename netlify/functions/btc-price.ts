af9af17cd76d61ceb9299718f7137241e5e13fe2
donatello {he/him} <53077361+lomar92@users.noreply.github.com>
2025-02-14 09:59:09 +0100

    Update btc-price.ts
    change from "https://api.coindesk.com/v1/bpi/currentprice.json" to --> "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur"

--- netlify/functions/btc-price.ts
+++ netlify/functions/btc-price.ts
@@ -33,9 +33,9 @@ export const handler: Handler = async (event, context) => {
     }
 
     // Fetch new price if cache is invalid or missing
-    const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur")
     const data = await response.json()
-    const eurPrice = data.bpi.EUR.rate_float
+    const eurPrice = data.bitcoin.eur
 
     // Update cache
     priceCache = {
